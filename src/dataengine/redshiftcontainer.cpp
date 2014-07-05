4a6177b51177034ad38e5d0df1697bf246c73498
Simone Gaiarin <simgunz@gmail.com>
2014-07-05 21:04:38 +0200

    Add doxygen comments to redshiftcontainer

--- src/dataengine/redshiftcontainer.cpp
+++ src/dataengine/redshiftcontainer.cpp
@@ -1,19 +1,25 @@
-/***************************************************************************
- *   Copyright (C) 2012 by Simone Gaiarin <simgunz@gmail.com>              *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, see <http://www.gnu.org/licenses/>.  *
- **************************************************************************/
+/************************************************************************
+* Copyright (C) 2012 by Simone Gaiarin <simgunz@gmail.com>              *
+*                                                                       *
+* This program is free software; you can redistribute it and/or modify  *
+* it under the terms of the GNU General Public License as published by  *
+* the Free Software Foundation; either version 3 of the License, or     *
+* (at your option) any later version.                                   *
+*                                                                       *
+* This program is distributed in the hope that it will be useful,       *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+* GNU General Public License for more details.                          *
+*                                                                       *
+* You should have received a copy of the GNU General Public License     *
+* along with this program; if not, see <http://www.gnu.org/licenses/>.  *
+************************************************************************/
+
+/*!
+ * \file redshiftcontainer.cpp
+ *
+ * Contains the implementation of the RedshiftContainer class.
+ */
 
 #include "redshiftcontainer.h"
 #include "redshiftservice.h"
@@ -26,6 +32,7 @@ RedshiftContainer::RedshiftContainer(QObject* parent)
     setObjectName("Controller");
     m_controller = new RedshiftController();
     QObject::connect(m_controller, SIGNAL(stateChanged(int, int)), this, SLOT(updateStatus(int, int)));
+    //Manually set the container data for the first time only
     updateStatus(m_controller->state(), m_controller->currentTemperature());
 }
 
@@ -42,6 +49,8 @@ void RedshiftContainer::updateStatus(int state, int temp)
         case 2: setData("Status", "RunningManual"); break;
     }
     setData("Temperature", temp);
+    //Check if any data is actually changed, and, if so, the dataUpdated signal is automatically emitted
+    //in order to inform the connected plasmoids of the change.
     checkForUpdate();
 }
 
