3d0a95a01d96d735f32d00a9831cb5c5a7cbb84b
Simone Gaiarin <simgunz@gmail.com>
2016-07-09 17:08:41 +0200

    Add basic structure for QML extension plugin
    References:
    
    * http://doc.qt.io/qt-5/qtqml-modules-cppplugins.html
    * http://doc.qt.io/qt-5/qtqml-tutorials-extending-qml-example.html

---
+++ src/applet/plugin/redshift.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright (C) 2015 by Bernhard Friedreich <friesoft@gmail.com>        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "redshift.h"
+#include "redshiftsettings.h"
+
+#include <QObject>
+
+Redshift::Redshift(QObject *parent) : QObject(parent)
+{
+
+}
+
+void Redshift::configAccepted()
+{
+//     QStringList alwaysOnActivities;
+//     QStringList alwaysOffActivities;
+
+//     QTreeWidget *activitiesList = m_activitiesUi->activities;
+//     for (int i = 0; i < activitiesList->topLevelItemCount(); ++i) {
+//         QTreeWidgetItem *item = activitiesList->topLevelItem(i);
+//         QComboBox *itemCombo = static_cast<QComboBox *>(activitiesList->itemWidget(item, 1));
+//         const QString act = item->data(0, Qt::UserRole).toString();
+//         if (itemCombo->currentIndex() == 1) {
+//             alwaysOnActivities << act;
+//         } else if (itemCombo->currentIndex() == 2) {
+//             alwaysOffActivities << act;
+//         }
+//     }
+//     RedshiftSettings::setAlwaysOnActivities(alwaysOnActivities);
+//     RedshiftSettings::setAlwaysOffActivities(alwaysOffActivities);
+    RedshiftSettings::self()->save();
+}
